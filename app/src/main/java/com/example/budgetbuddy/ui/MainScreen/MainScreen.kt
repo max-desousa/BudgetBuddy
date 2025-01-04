91897405c8cfbc537f48b02401a6dacfb267b212
Max DeSousa <maxdesousa@protonmail.com>
2025-01-04 16:41:24 -0500

    Some old commit about keyboards.
    This commit seems to be related to changing the default keybooard
    to the numbers only version.
    
    Commiting to continue database work with clean environment.

--- app/src/main/java/com/example/budgetbuddy/ui/MainScreen/MainScreen.kt
+++ app/src/main/java/com/example/budgetbuddy/ui/MainScreen/MainScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Card
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
@@ -17,6 +18,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -144,6 +146,7 @@ fun EntryCard(modifier : Modifier = Modifier) {
                 value = "Test",
                 onValueChange = {},
                 singleLine = true,
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
                 modifier = Modifier
                     .fillMaxWidth()
             )
